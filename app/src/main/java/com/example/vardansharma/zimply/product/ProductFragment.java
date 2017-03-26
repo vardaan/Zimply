b44f0d927deb67cb9c44cb006a281959be5d950f
vardansharma <vardan.sharma@bookmyshow.com>
2017-03-26 18:38:30 +0530

    added a product list with basic ui

--- app/src/main/java/com/example/vardansharma/zimply/product/ProductFragment.java
+++ app/src/main/java/com/example/vardansharma/zimply/product/ProductFragment.java
@@ -1,20 +1,33 @@
 package com.example.vardansharma.zimply.product;
 
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.vardansharma.zimply.R;
 import com.example.vardansharma.zimply.base.BaseFragment;
+import com.example.vardansharma.zimply.utils.Utils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductFragment extends BaseFragment {
 
+
+    @BindView (R.id.product_list_rv)
+    RecyclerView productRv;
+    @BindView (R.id.product_list_fab)
+    FloatingActionButton fab;
 
     public ProductFragment() {
         // Required empty public constructor
@@ -25,7 +38,54 @@ public class ProductFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false);
+        View view = inflater.inflate(R.layout.fragment_product, container, false);
+        ButterKnife.bind(this, view);
+        final int space = (int) Utils.dpToPixel(16, getActivity());
+        productRv.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        productRv.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                final int position = parent.getChildAdapterPosition(view);
+                if (position % 2 == 0) {
+                    outRect.left = space;
+                    outRect.right = space / 2;
+                }
+                else {
+                    outRect.left = space / 2;
+                    outRect.right = space;
+                }
+                outRect.top = space;
+            }
+        });
+        productRv.setAdapter(new ProductAdapter());
+        return view;
+    }
+
+    class ProductAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            final View view
+                    = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
+            return new ProductVH(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+        class ProductVH extends RecyclerView.ViewHolder {
+
+            public ProductVH(View itemView) {
+                super(itemView);
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/vardansharma/zimply/utils/Utils.java
+++ app/src/main/java/com/example/vardansharma/zimply/utils/Utils.java
@@ -1,8 +1,11 @@
 package com.example.vardansharma.zimply.utils;
 
 import android.content.ContentUris;
+import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
 
 import static java.lang.Long.parseLong;
 
@@ -20,4 +23,12 @@ public class Utils {
         Preconditions.checkNotNull(id, "album id is null");
         return ContentUris.withAppendedId(ART_CONTENT_URI, parseLong(id));
     }
+
+    public static float dpToPixel(float dp, Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px;
+    }
+
 }
